b15c3282f8bb8bf1be19a47da6c2e63e6000e8d0
takuwaaan <50600390+takuwaaan@users.noreply.github.com>
2020-01-14 15:56:45 +0900

    Update hane.py

--- hane.py
+++ hane.py
@@ -13,15 +13,12 @@ data_z_add = np.append(data,z,axis=2)
 ###Z軸変更プログラム###
 change_line = input("変更したい画数は：")
 line = int(change_line)
-print(data_z_add[line][0])
-print(data_z_add[line][0][0])
 
 #lineのデータ数を取得
 c=0
 for i in range(1000):
     if data_z_add[line][i][0] != -100:
         c+=1
-print(c)
 
 #Z軸の値を変更
 #2/3で値を変更
