2fb8298985aaf5f6c7a8d5918edfe0a8d66d88fa
takuwaaan <50600390+takuwaaan@users.noreply.github.com>
2020-01-15 10:49:46 +0900

    Add files via upload

--- tomehane_flag.py
+++ tomehane_flag.py
@@ -1,6 +1,7 @@
 import numpy as np
 np.set_printoptions(threshold=np.inf)
-data = np.load("a_test.npy")
+s = input("読み込むデータは？：")
+data = np.load(s+".npy")
 
 ###data準備###
 #Z軸dataの準備・追加 -100のz軸を元データに追加#
@@ -9,38 +10,36 @@ z = np.full(((kaku_all[0], 1000, 1)), -100)
 data_z_add = np.append(data,z,axis=2)
 
 ###Z軸変更プログラム###
-change_line = input("変更したい画数は：")
-kind_line = input("とめ:0 はらい:1 を入力：")
-line = int(change_line)
-k_line = int(kind_line)
+for line in range(kaku_all[0]):
+    #lineのデータ数を取得
+    c=0
+    for i in range(1000):
+        if data_z_add[line][i][0] != -100:
+            c+=1
 
-#lineのデータ数を取得
-c=0
-for i in range(1000):
-    if data_z_add[line][i][0] != -100:
-        c+=1
-
-#Z軸の値を変更
-
-#はらい flag=1
-#2/3で値を変更
-if k_line == 1:
-    change_pt = int(2 / 3 * c)
-    for i in range(c):
-        if i <= change_pt:
-            data_z_add[line][i][2] = 0
-        else:
-            data_z_add[line][i][2] = 1
-#とめ flag=2
-elif k_line == 0:
-    end_pt = c-20
-    for i in range(c):
-        if i <= end_pt:
-            data_z_add[line][i][2] = 0
-        else:
-            data_z_add[line][i][2] = 2
-    for i in range(end_pt*(-1)+c):
-        data_z_add[line][c+i][0] = data_z_add[line][c-i-1][0]
-        data_z_add[line][c+i][1] = data_z_add[line][c-i-1][1]
-        data_z_add[line][c+i][2] = 2
-print(data_z_add[line])
+    #Z軸の値を変更
+    #はらい flag=1
+    #2/3で値を変更
+    kind_line = input(str(line+1)+"画目はとめ？はらい？（とめ:0 はらい:1 を入力）：")
+    k_line = int(kind_line)
+    if k_line == 1:
+        change_pt = int(2 / 3 * c)
+        for i in range(c):
+            if i <= change_pt:
+                data_z_add[line][i][2] = 0
+            else:
+                data_z_add[line][i][2] = 1
+    #とめ flag=2
+    elif k_line == 0:
+        end_pt = c-20
+        for i in range(c):
+            if i <= end_pt:
+                data_z_add[line][i][2] = 0
+            else:
+                data_z_add[line][i][2] = 2
+        for i in range(end_pt*(-1)+c):
+            data_z_add[line][c+i][0] = data_z_add[line][c-i-1][0]
+            data_z_add[line][c+i][1] = data_z_add[line][c-i-1][1]
+            data_z_add[line][c+i][2] = 2
+ans = s+"_plus_flag"
+np.save(ans,data_z_add)
